63c7bc50350cd48f1a41c6fbf260d97c38f98de7
bxhneodfk <bxhneodfk@gmail.com>
2021-06-24 22:21:05 +0800

    remove some code

--- contracts/interface/IBGMBackup.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.12;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface IBGMBackup{
-    function profit() external returns(uint256 amountOut,uint256 amountIn);
-}
-
-
